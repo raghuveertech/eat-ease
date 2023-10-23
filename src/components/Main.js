c61b8287707b8984861732294698d03fc5888ebb
raghuveertech <raghuveerp25@gmail.com>
2023-10-23 22:11:27 +0530

    shimmer added to restaurants list

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,17 @@ import "./../scss/main.scss";
 import Filters from "./Filters";
 import Restaurants from "./Restaurants";
 import Search from "./Search";
-import { restaurants as initialRestaurants } from "../utils/mockData";
+import Shimmer from "./Shimmer";
 import { useEffect, useState } from "react";
 
 const Main = () => {
-  const [restaurants, setRestaurants] = useState(initialRestaurants);
+  const [restaurants, setRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [filters, setFilters] = useState({
     topRated: false,
     pureVeg: false,
   });
+  const [apiCallComplete, setApiCallComplete] = useState(false);
 
   const filtersHandle = (e, filterItem, isSet) => {
     e.stopPropagation();
@@ -21,28 +23,62 @@ const Main = () => {
   };
 
   const filterRestaurants = () => {
-    let filteredRestaurants = [...initialRestaurants];
+    let filteredRestaurants = [...restaurants];
     filteredRestaurants = filteredRestaurants.filter((item) => {
       if (filters.pureVeg === true && !item.info.veg) {
         return false;
       }
-      if (filters.topRated === true && item.info.avgRating <= 4) {
+      if (filters.topRated === true && item.info.avgRating <= 4.2) {
         return false;
       }
       return true;
     });
-    setRestaurants(filteredRestaurants);
+    setFilteredRestaurants(filteredRestaurants);
   };
 
   useEffect(() => {
     filterRestaurants();
   }, [filters]);
 
+  const fetchRestaurants = async () => {
+    try {
+      const dataR = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.470984962807403&lng=78.54666475206614"
+      );
+      const json = await dataR.json();
+      const allCards = json.data.cards;
+      allCards.forEach((card) => {
+        if (
+          card?.card?.card?.gridElements?.infoWithStyle?.restaurants?.length >
+          10
+        ) {
+          let restaurantsFromAPI =
+            card?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+          setRestaurants(restaurantsFromAPI);
+          setFilteredRestaurants(restaurantsFromAPI);
+          return false;
+        }
+      });
+      setApiCallComplete(true);
+    } catch (err) {
+      console.log(err);
+      setApiCallComplete(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
   return (
     <div className="main">
       <Search />
       <Filters filters={filters} filtersHandle={filtersHandle} />
-      <Restaurants restaurants={restaurants} />
+      {!apiCallComplete && restaurants.length === 0 ? (
+        <Shimmer />
+      ) : (
+        <Restaurants restaurants={filteredRestaurants} />
+      )}
     </div>
   );
 };
---
+++ src/components/Shimmer.js
@@ -0,0 +1,37 @@
+import "./../scss/shimmer.scss";
+
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>{" "}
+      <div className="shimmer-card">
+        <div className="shimmer-card-inner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
